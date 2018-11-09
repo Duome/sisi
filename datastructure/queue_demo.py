84c5fa501f35574decb4d2a57b504acd8d1ceed6
Duome <duomesiki@163.com>
2018-11-09 21:50:28 +0800

    #1968 修改数据结构栈和队列
    Signed-off-by: Duome <duomesiki@163.com>

--- datastructure/queue_demo.py
+++ datastructure/queue_demo.py
@@ -1,57 +1,67 @@
 # -*- coding: utf-8 -*-
-#从头出，从尾进
-class Squeue(object):
+#从头进，从尾出，列表的头是队列的尾，列表的尾是队列的头
+class SQueue(object):
 
     def __init__(self):
-        self.queue = []
+        self._item = []
 
     def is_empty(self):
-        return not self.queue
+        return not self._item
 
     def dnqueue(self):
         assert not self.is_empty()
-        return self.queue.pop(0)
+        return self._item.pop(0)
 
     def enqueue(self, item):
-        self.queue.append(item)
-
-class Lqueue(object):
+        self._item.append(item)
+#出队操作是rear什么意思？这里的rear是指指针
+class LQueue(object):
     def __init__(self):
-        self.front = None
+        self._front = None
         self.rear = None
         self.size = 0
 
     def is_empty(self):
         return not self.size
 
-    def pop(self):
+    def dnqueue(self):
         assert not self.is_empty()
-        info = self.front.item
-        self.front.next = self.front
-        return info
+        info = self._front
+        self._front = info.next
+        return info.item
 
-    def prepare(self, item):
+    def enqueue(self, item):
         pin = Node(item)
         if self.is_empty():
-            self.front = self.rear = pin
+            self._front = self.rear = pin
         else:
-            self.rear.next = pin
+            # 为什么这里使用self.rear.next会报AttributeError: can't set attribute
+            self.rear._next = pin
             self.rear = pin
         self.size += 1
 
 
 class Node(object):
     def __init__(self, item):
-        self.item = item
-        self.next = None
+        self._item = item
+        self._next = None
+
+    @property
+    def item(self):
+        return self._item
+
+    @property
+    def next(self):
+        return self._next
 
 if __name__ == '__main__':
-    q = Lqueue()
+    q = LQueue()
     print(q.is_empty())
-    q.prepare(1)
+    q.enqueue(1)
+    print(q._front.next)
     print(q.is_empty())
-    q.prepare(2)
+    q.enqueue(2)
     print(q.is_empty())
-    q.pop()
-    q.pop()
+    print(q.dnqueue())
+    print(q.dnqueue())
     print(q.is_empty())
--- datastructure/stack_demo.py
+++ datastructure/stack_demo.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-class Sstack(object):
+class SStack(object):
     def __init__(self):
-        self.stack = []
+        self._items = []
 
     def is_empty(self):
-        return not self.stack
+        return not self._items
 
     def pop(self):
         assert self.is_empty()
-        info = self.stack.pop()
+        info = self._items.pop()
         return info
 
     def push(self, item):
-        self.stack.append(item)
+        self._items.append(item)
 
-# 从尾进，从尾出
-class Lstack(object):
+# 从栈尾进，从栈尾出，栈头头列表的尾
+class LStack(object):
     def __init__(self):
         self.top = None
         self.size = 0
@@ -23,7 +23,7 @@ class Lstack(object):
     def is_empty(self):
         return not self.size
 
-    def prepare(self, item):
+    def push(self, item):
         # 后一个指针的next属性即为上一个指针
         self.top = Node(item, self.top)
         self.size += 1
@@ -37,5 +37,13 @@ class Lstack(object):
 
 class Node(object):
     def __init__(self, item, next_=None):
-        self.item = item
-        self.next_ = next_
+        self._item = item
+        self._next = next_
+
+    @property
+    def item(self):
+        return self._item
+
+    @property
+    def next(self):
+        return self._next
